82365b5ed4b457b4ad83afd968d3c013b917f0b9
SALHI-1 <mohammedsalhisim@gmail.com>
2026-06-25 23:50:15 +0100

    first commit

---
+++ app/Models/Product.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'category_id', 'name', 'description', 'price', 'stock', 
+        'volume', 'is_new', 'is_bestseller'
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'is_new' => 'boolean',
+            'is_bestseller' => 'boolean',
+            'price' => 'decimal:2',
+        ];
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
